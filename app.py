101922b29fd3725b4deb8f1cb178e966be41896d
Himanshu <himanshu290304@gmail.com>
2025-08-12 01:15:51 +0530

    firecrawl api is running good

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from firecrawl import FirecrawlApp 
 
 st.title("BlogCast")
 
@@ -6,8 +7,18 @@ url = st.text_input("Enter the blog url :")
 
 if st.button("Generate Podcast"):
     if url:
-        st.write("Url received")
-        st.info("Processing starts")
+        try:
+            app = FirecrawlApp(api_key = st.secrets["FIRECRAWL_API_KEY"])
+
+            st.info("Scraping Blog Content...")
+            
+            scrape_result = app.scrape_url(url=url)
+
+            st.subheader("Scraped Content (as Markdown):")
+            st.markdown(scrape_result.markdown)
+
+        except Exception as e:
+            st.error(f"An error occured : {e}")
 
     else:
-        st.error("Incorrect Url")
+        st.error("Enter the URL First....")
